6b9b9dd3f145e018573576327cd4824d248daeac
titiwister <thibaut.coudron@outscale.com>
2026-08-26 17:38:14 +0200

    add: postgresql port to postgesql sg

--- sg_k8s.tf
+++ sg_k8s.tf
@@ -44,3 +44,14 @@ resource "outscale_security_group_rule" "k8s_kapi" {
     ip_ranges       = ["${var.haproxy_ip}/32"]
   }
 }
+
+resource "outscale_security_group_rule" "postgresql_postgre" {
+  flow              = "Inbound"
+  security_group_id = outscale_security_group.sg_haproxy.security_group_id
+  rules {
+    from_port_range = "5432"
+    to_port_range   = "5433"
+    ip_protocol     = "tcp"
+    ip_ranges       = ["${var.k8s_subnet}"]
+  }
+}
